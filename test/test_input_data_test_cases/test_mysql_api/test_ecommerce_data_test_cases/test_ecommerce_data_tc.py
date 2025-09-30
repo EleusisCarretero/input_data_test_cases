9a9c7f0b973fddc5b0932775ec4ae1d6aa85507b
EleusisCarretero <eleusis.carretero@gmail.com>
2025-09-29 20:56:48 -0600

    feat: include home and definequeries methods in unit test

--- test/test_input_data_test_cases/test_mysql_api/test_ecommerce_data_test_cases/test_ecommerce_data_tc.py
+++ test/test_input_data_test_cases/test_mysql_api/test_ecommerce_data_test_cases/test_ecommerce_data_tc.py
@@ -10,11 +10,32 @@ class TestEcommerceDataTC(unittest.TestCase):
     def setUp(self):
         self.api = EcommerceDataTC(config={})
         self.app = self.api.app
+    
+    @patch.object(EcommerceDataTC, "format_response")
+    def test_home(self, patch_format):
+        base_url_msg = {'message': "Base url"}
+        patch_format.side_effect = lambda payload: jsonify(payload)
+        with self.app.test_request_context("/"):
+            resp = self.api.home()
+
+        patch_format.assert_called_once_with(base_url_msg)
+        data = resp.get_json()
+        self.assertEqual(data, base_url_msg)
+
+    def test_define_queries(self):
+        expected_map_quieries = {
+            'GET_TESTCASE_PARAMS': 'select params from parameters where {column} = "{value}"',
+            'INVALID_KEY': None
+        }
+        for key, expect_query in expected_map_quieries.items():
+            actual_query = self.api.define_queries(key)
+            self.assertEqual(actual_query, expect_query)
+
 
     @patch.object(EcommerceDataTC, "format_response")
     @patch.object(EcommerceDataTC, "define_queries")
     @patch.object(EcommerceDataTC, "connect_data_base")
-    def test_get_test_case_invalid_requests(self, patch_connect_db, patch_queries,patch_format):
+    def test_get_test_case_invalid_requests(self, patch_connect_db, patch_queries, patch_format):
         endpoint_error_msg = {
             '/test_case': "Missing id or testcase name",
             '/test_case?id=1&name=test_case_dummy': "You just can choose id or name, not both",
@@ -41,7 +62,7 @@ class TestEcommerceDataTC(unittest.TestCase):
     @patch.object(EcommerceDataTC, "format_response")
     @patch.object(EcommerceDataTC, "define_queries")
     @patch.object(EcommerceDataTC, "connect_data_base")
-    def test_get_test_case_valid_requests(self, patch_connect_db, patch_queries,patch_format):
+    def test_get_test_case_valid_requests(self, patch_connect_db, patch_queries, patch_format):
         endpoint_response = {
             '/test_case?id=1': {'params': [{'timeout': 2}]},
             '/test_case?name=test_timeout': {'params': [{'timeout': 2}]},
@@ -64,11 +85,10 @@ class TestEcommerceDataTC(unittest.TestCase):
             patch_connect_db.reset_mock()
             patch_queries.reset_mock()
 
-
     @patch.object(EcommerceDataTC, "format_response")
     @patch.object(EcommerceDataTC, "define_queries")
     @patch.object(EcommerceDataTC, "connect_data_base")
-    def test_get_test_case_raise_exception(self, patch_connect_db, patch_queries,patch_format):
+    def test_get_test_case_raise_exception(self, patch_connect_db, patch_queries, patch_format):
         patch_connect_db.side_effect = MyslApiException("Unable to execute command")
         patch_format.side_effect = lambda payload: jsonify(payload)
         patch_format.return_value = {'message': 'Unable to get the desired test case'}
@@ -78,4 +98,3 @@ class TestEcommerceDataTC(unittest.TestCase):
         patch_queries.assert_called_once_with(key='GET_TESTCASE_PARAMS')
         data = resp.get_json()
         self.assertEqual(data.get('message'), 'Unable to get the desired test case')
-            

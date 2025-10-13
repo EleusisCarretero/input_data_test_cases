33c084a1e94a27cb33e1097933a379e01c03b88d
EleusisCarretero <eleusis.carretero@gmail.com>
2025-10-13 12:39:49 -0600

    feat: include post

--- input_data_test_cases/mysql_api/ecommerce_data_test_cases/ecommerce_data_tc.py
+++ input_data_test_cases/mysql_api/ecommerce_data_test_cases/ecommerce_data_tc.py
@@ -1,4 +1,5 @@
 from flask import request
+from input_data_test_cases.base_api import StatusCode
 from input_data_test_cases.mysql_api.mysql_api import MysqlApi, MyslApiException
 
 
@@ -12,44 +13,51 @@ class EcommerceDataTC(MysqlApi):
 
     def define_routes(self):
         self.app.add_url_rule("/",view_func=self.home)
-        self.app.add_url_rule("/test_case",endpoint="test_case",view_func=self.test_case,methods=["GET", "POST"])
-        #self.app.add_url_rule("/test_case",endpoint="post_test_case",view_func=self.post_test_case,methods=["POST"])
+        self.app.add_url_rule("/test_case",endpoint="get_test_case",view_func=self.get_test_case,methods=["GET"])
+        self.app.add_url_rule("/test_case",endpoint="post_test_case",view_func=self.post_test_case,methods=["POST"])
 
     def define_queries(self, key):
         return {
-            'GET_TESTCASE_PARAMS': 'select params from parameters where {column} = "{value}"'
+            'GET_TESTCASE_PARAMS': 'select params from parameters where {column} = "{value}"',
+            'POST_TESTCASE_PARAMS': 'INSERT INTO parameters ({columns}) VALUES ({placeholders})'
         }.get(key, None)
 
     def home(self):
-        return self.format_response({'message': "Base url"})
+        return self.format_response({'message': "Base url"}, status_code=StatusCode.OK)
 
-    def test_case(self):
-        if request.method == "GET":
-            self.get_test_case()
-        elif request.method == "POST":
-            self.post_test_case()
-        else:
-            pass
     def post_test_case(self):
-        print("POST query")
+        name = request.form.get('name', None)
+        params = request.form.get('params', None)
+        columns = "name, params"
+        values =  [name, str(params)]
+        placeholders = ",".join(["%s"] * len(values))  # %s,%s,%s
+        base_command = self.define_queries(key='POST_TESTCASE_PARAMS')
+        response = self.connect_data_base(
+            command=base_command.format(
+                columns=columns,
+                placeholders=placeholders
+            ),
+            extra=values)
+        status_code = StatusCode.OK
+        return self.format_response(response, status_code=status_code)
 
     def get_test_case(self):
         response = {}
         id = request.args.get('id', None)
         name = request.args.get('name', None)
         if id is None and name is None:
-            return self.format_response({'message': "Missing id or testcase name"})
+            return self.format_response({'message': "Missing id or testcase name"}, status_code=StatusCode.BAD_REQUEST)
         elif id and name:
-            return self.format_response({'message': "You just can choose id or name, not both"})
+            return self.format_response({'message': "You just can choose id or name, not both"}, status_code=StatusCode.BAD_REQUEST)
         elif id:
             try:
                 int(id)
             except ValueError:
-                return self.format_response({'message': "ID should be an integer"})
+                return self.format_response({'message': "ID should be an integer"}, status_code=StatusCode.BAD_REQUEST)
         elif name:
             try:
                 int(name)
-                return self.format_response({'message': "Name should be a string"})
+                return self.format_response({'message': "Name should be a string"}, status_code=StatusCode.BAD_REQUEST)
             except ValueError:
                 pass
         value = id or name
@@ -57,9 +65,11 @@ class EcommerceDataTC(MysqlApi):
         try:
             base_command = self.define_queries(key='GET_TESTCASE_PARAMS')
             response = self.connect_data_base(command=base_command.format(column=column,value=value))
+            status_code = StatusCode.OK
         except MyslApiException:
             response = {'message': 'Unable to get the desired test case'}
-        return self.format_response(response)
+            status_code = StatusCode.NOT_FOUND
+        return self.format_response(response, status_code=status_code)
 
 
 

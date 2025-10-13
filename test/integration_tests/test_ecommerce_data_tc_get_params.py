6caa6fcef07afa2396ed943ca838a6e17deaca79
EleusisCarretero <eleusis.carretero@gmail.com>
2025-10-12 21:20:22 -0600

    style: encapsulate task

--- test/integration_tests/test_ecommerce_data_tc_get_params.py
+++ test/integration_tests/test_ecommerce_data_tc_get_params.py
@@ -8,7 +8,14 @@ import yaml
 import os
 import json
 
+
+class TestEcommerceDataTCGetParamsException(Exception):
+    pass
+
+
 class TestEcommerceDataTCGetParams():
+    CREATE_TABLE_BASE_QUERY = "CREATE TABLE IF NOT EXISTS {table_name}({columns_to_insert})"
+    INSERT_NEW_VALUE_BASE_QUERY = "INSERT INTO parameters ({columns}) VALUES ({placeholders})"
 
     @pytest.fixture(autouse=True)
     def setup_db(self):
@@ -53,20 +60,26 @@ class TestEcommerceDataTCGetParams():
                 if time.time() - start > max_seconds:
                     raise
                 time.sleep(1)
-    
-    def _import_test_data(self, test_data_file):
-        full_path = os.path.join(os.path.dirname(__file__), test_data_file)
-        with open(full_path, 'r') as f:
-            data = yaml.load(f, Loader=yaml.SafeLoader)
-        return data
-    
 
     def init_database(self, table, data):
-        cur = self.mysql.connection.cursor()
-        table_name = table['name']
-        columns = table['columns']
-        create_table_command = f"CREATE TABLE IF NOT EXISTS {table_name}"
-        fields = ""
+
+        try:
+            cur = next(self._handle_cursor())
+        except Exception as e:
+            raise TestEcommerceDataTCGetParamsException("Unable to create cursor") from e
+
+        self._create_db(
+            cur=cur,
+            table_name=table['name'],
+            columns=table['columns']
+        )
+        self._insert_values_db(
+            cur=cur,
+            data=data
+        )
+    
+    def _create_db(self, cur, table_name, columns):
+        columns_to_insert = ""
         for i, column in enumerate(columns):
             cmd = " ".join([column['name'], column['type']])
             if column['type'].upper() == 'VARCHAR':
@@ -74,15 +87,18 @@ class TestEcommerceDataTCGetParams():
             if column.get('primary', False):
                 cmd+= ' PRIMARY KEY'
             if i < len(columns) - 1:
-                fields += cmd + ","
+                columns_to_insert += cmd + "," 
             else:
-                fields += cmd
-        create_table_command += "(" + fields + ")"
-        cur.execute(
-            create_table_command
+                columns_to_insert += cmd
+        self._query_cmd(
+            cur=cur,
+            base_query=self.CREATE_TABLE_BASE_QUERY,
+            kwarg={'table_name':table_name, 'columns_to_insert':columns_to_insert},
         )
+    
+    def _insert_values_db(self, cur, data):
         for datum in data:
-            fields = ",".join(map(str, list(datum.keys())))
+            columns = ",".join(map(str, list(datum.keys())))
             values = []
             for v in datum.values():
                 if isinstance(v, (dict, list)):
@@ -90,12 +106,32 @@ class TestEcommerceDataTCGetParams():
                 else:
                     values.append(v)
             placeholders = ",".join(["%s"] * len(values))  # %s,%s,%s
-            cmd = f"INSERT INTO parameters ({fields}) VALUES ({placeholders})"
-    
-            cur.execute(cmd, tuple(values))
-            self.mysql.connection.commit()
+            self._query_cmd(
+                cur=cur,
+                base_query=self.INSERT_NEW_VALUE_BASE_QUERY,
+                kwarg={'columns':columns, 'placeholders':placeholders},
+                extra=values
+            )
+
+    def _handle_cursor(self):
+        cur = self.mysql.connection.cursor()
+        yield cur
         cur.close()
-    
+
+    def _query_cmd(self, cur, base_query, kwarg, extra=None):
+        cmd = base_query.format(**kwarg)
+        if extra:
+            cur.execute(cmd, tuple(extra))
+        else:
+            cur.execute(cmd)
+        self.mysql.connection.commit()
+
+    def _import_test_data(self, test_data_file):
+        full_path = os.path.join(os.path.dirname(__file__), test_data_file)
+        with open(full_path, 'r') as f:
+            data = yaml.load(f, Loader=yaml.SafeLoader)
+        return data
+
     def init_docker_compose(self):
         compose_path = os.path.abspath("docker-compose.yaml")
         subprocess.check_output(['docker', 'compose', '-f', compose_path, 'up', '-d'])

da3eef592bad46c730b3350c7a9852ebf4bc0b17
EleusisCarretero <eleusis.carretero@gmail.com>
2026-01-09 18:17:42 -0600

    feat: change defualt port

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ def load_mysql_config(mode: str) -> dict:
             "MYSQL_USER": os.environ["DB_USER"],
             "MYSQL_PASSWORD": os.environ["DB_PASS"],
             "MYSQL_HOST": os.environ["DB_HOST"],
-            "MYSQL_PORT": int(os.getenv("DB_PORT", "3306")),
+            "MYSQL_PORT": int(os.getenv("DB_PORT", "17494")),
             "MYSQL_DB": os.environ["DB_NAME"],
         }
     else:
@@ -20,6 +20,6 @@ def load_mysql_config(mode: str) -> dict:
         }
 
 
-mode = os.getenv("DB_MODE", "local")
+mode = os.getenv("DB_MODE", "remote")
 
 app = EcommerceDataTC(load_mysql_config(mode)).app
